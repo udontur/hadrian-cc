4f702588face83d692092de6e26d07903b865ad3
udontur <h@hadrian.cc>
2025-04-12 17:57:01 +0800

    feat: 404 Page

---
+++ app/404.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Custom404() {
+    return (
+        <>
+            <meta http-equiv="refresh" content="0; url=https://hadrian.cc"/>
+            <div>404 Not Found, Redirecting to the home page.</div>
+        </>
+    );
+}
