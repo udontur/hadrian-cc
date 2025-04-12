c1c6df53db70eaeee476092b400105ba419469aa
udontur <h@hadrian.cc>
2025-04-12 17:51:21 +0800

    feat: Comments

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,26 @@
+// Content
+import Landing from "./page/Landing.tsx";
+import Linktree from "./page/Linktree.tsx";
 import Contests from "./page/content/Contests.tsx";
 import Techstack from "./page/content/Techstack.tsx";
 import Experiences from './page/content/Experiences.tsx';
 import Projects from "./page/content/Projects.tsx";
 import Awards from "./page/content/Awards.tsx";
 
+// Website
 import Navbar from "./page/Navbar.tsx";
 import Footer from "./page/Footer.tsx";
-import Landing from "./page/Landing.tsx";
 import Empty from "./page/Empty.tsx";
-import Linktree from "./page/Linktree.tsx";
 import Divider from "./page/Divider.tsx";
 
+// Vercel
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <>
+    
         <Navbar/>
         <Empty/>
         <main>
@@ -34,7 +38,6 @@ export default function Home() {
                 <Hardskill/> (Replace tech stack?)
                 */}
                 <Awards/>
-                
                 {/* contactme */}
             </div>
             <div className="m-32"></div>
@@ -43,6 +46,7 @@ export default function Home() {
         <Empty/>
         <Footer/>
 
+        {/* Vercel */}
         <Analytics/>
         <SpeedInsights/>
     </>
