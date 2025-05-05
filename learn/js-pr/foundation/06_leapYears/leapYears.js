e6a66b158ecd339ed449524878df1e403595c4f7
udontur <h@hadrian.cc>
2025-05-05 18:19:51 +0800

    legacy

--- learn/js-pr/foundation/06_leapYears/leapYears.js
+++
@@ -1,9 +0,0 @@
-const leapYears = function(currentYear) {
-    
-    if(currentYear%4==0&&currentYear%100!=0) return true;
-    if(currentYear%400==0) return true;
-    return false;
-};
-
-// Do not edit below this line
-module.exports = leapYears;
