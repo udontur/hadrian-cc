32d0970bd57a41e942e9ccfe35b0ece949f6037a
udontur <h@hadrian.cc>
2025-07-13 15:15:44 +0800

    fix: jumps

--- src/page/components/Socials.jsx
+++ src/page/components/Socials.jsx
@@ -3,7 +3,7 @@
 
 function Socials({link, svg}) {
     return (
-        <a className="no-under" target="_blank" href={link}>
+        <a className="no-under" href={link}>
             <button className="socials">
                 <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
                     <path fill="#fff" d={svg} />
