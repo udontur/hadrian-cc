ec0b1524690aa1889da700c160a14eb3283c2fa4
Hadrian <h@hadrian.cc>
2026-02-19 19:53:38 +0800

    feat: font api

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    experimental: {
+        fonts: [{
+            provider: fontProviders.google(),
+            name: "Manrope",
+            cssVariable: "--font-manrope"
+        }]
+    },
     vite: {
         plugins: [tailwindcss()]
     }
