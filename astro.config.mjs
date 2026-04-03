39948a8fc7fe3a2843a845020cb07c1a7ec9059c
Hadrian <h@hadrian.cc>
2026-04-03 19:45:18 +0800

    why nothing works

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,29 @@
 import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        fonts: [
-            {
-                provider: fontProviders.google(),
-                name: "Manrope",
-                cssVariable: "--font-manrope"
-            },
-            {
-                provider: fontProviders.google(),
-                name: "Shantell Sans",
-                cssVariable: "--font-shantell"
-            }
-        ],
-        svgo: true
-    },
-    vite: {
-        plugins: [tailwindcss()]
-    }
+  experimental: {
+      fonts: [
+          {
+              provider: fontProviders.google(),
+              name: "Manrope",
+              cssVariable: "--font-manrope"
+          },
+          {
+              provider: fontProviders.google(),
+              name: "Shantell Sans",
+              cssVariable: "--font-shantell"
+          }
+      ],
+      svgo: true
+  },
+
+  vite: {
+      plugins: [tailwindcss()]
+  },
+
+  integrations: [react()]
 });
