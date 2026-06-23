e8740a3bd48a9e354ee3a7aeabc6dab7ae57f176
Hadrian <h@hadrian.cc>
2026-06-23 22:32:47 +0800

    chore: biome check

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,28 +6,28 @@ import { defineConfig, fontProviders } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-    fonts: [
-      {
-        provider: fontProviders.fontsource(),
-        name: "Manrope",
-        cssVariable: "--font-sans",
-      },
-      {
-        provider: fontProviders.fontsource(),
-        name: "Shantell Sans",
-        cssVariable: "--font-heading",
-      },
-      {
-        provider: fontProviders.fontsource(),
-        name: "Jersey 15",
-        cssVariable: "--font-mono",
-      },
-      {
-        provider: fontProviders.fontsource(),
-        name: "WDXL Lubrifont TC",
-        cssVariable: "--font-chinese",
-      },
-    ],
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Manrope",
+      cssVariable: "--font-sans",
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Shantell Sans",
+      cssVariable: "--font-heading",
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Jersey 15",
+      cssVariable: "--font-mono",
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "WDXL Lubrifont TC",
+      cssVariable: "--font-chinese",
+    },
+  ],
 
   vite: {
     plugins: [tailwindcss()],
