8132458fbb45b3d049140f5dc51235f2696cc9a3
Hadrian <h@hadrian.cc>
2026-06-23 22:34:55 +0800

    chore: biome formatting

--- src/components/wrapper/HeroText.tsx
+++ src/components/wrapper/HeroText.tsx
@@ -3,62 +3,62 @@ import { Signature } from "@/components/ui/signature.tsx"
 import { SpecialText } from "@/components/ui/special-text.tsx"
 
 export function SignatureName({
-  text,
-  size,
-  color,
-  duration,
+    text,
+    size,
+    color,
+    duration,
 }: {
-  text: string
-  size: number
-  color: string
-  duration: number
+    text: string
+    size: number
+    color: string
+    duration: number
 }) {
-  return (
-    <div>
-      <Signature
-        text={text}
-        fontSize={size}
-        color={color}
-        duration={duration}
-        height={size}
-        inView
-      />
-    </div>
-  )
+    return (
+        <div>
+            <Signature
+                text={text}
+                fontSize={size}
+                color={color}
+                duration={duration}
+                height={size}
+                inView
+            />
+        </div>
+    )
 }
 
 export function SecondName({
-  text,
-  className = "",
-  speed,
+    text,
+    className = "",
+    speed,
 }: {
-  text: string
-  speed?: number
-  className?: string
+    text: string
+    speed?: number
+    className?: string
 }) {
-  return (
-    <div>
-      <BlurReveal className={className} speedReveal={speed} inView>
-        {text}
-      </BlurReveal>
-    </div>
-  )
+    return (
+        <div>
+            <BlurReveal className={className} speedReveal={speed} inView>
+                {text}
+            </BlurReveal>
+        </div>
+    )
 }
 
 export function UserNameFlash({
-  text,
-  className = "",
-  speed,
+    text,
+    className = "",
+    speed,
 }: {
-  text: string
-  className?: string
-  speed: number
+    text: string
+    className?: string
+    speed: number
 }) {
-  return (
-    <div>
-      <SpecialText className={className} speed={speed} inView>
-        {text}
-      </SpecialText>
-    </div>
-  )
+    return (
+        <div>
+            <SpecialText className={className} speed={speed} inView>
+                {text}
+            </SpecialText>
+        </div>
+    )
 }
