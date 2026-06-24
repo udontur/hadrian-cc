8388002ec8ba3cb8a17f0bd079f9361164b18a50
Hadrian <h@hadrian.cc>
2026-06-24 09:33:46 +0800

    chore: remove more font and add padding

---
+++ src/components/shadcn/drawer.tsx
@@ -0,0 +1,66 @@
+import * as React from "react"
+import { Drawer as DrawerPrimitive } from "vaul"
+
+import { cn } from "@/lib/utils"
+
+function Drawer({
+    ...props
+}: React.ComponentProps<typeof DrawerPrimitive.Root>) {
+    return <DrawerPrimitive.Root data-slot="drawer" {...props} />
+}
+
+function DrawerTrigger({
+    ...props
+}: React.ComponentProps<typeof DrawerPrimitive.Trigger>) {
+    return <DrawerPrimitive.Trigger data-slot="drawer-trigger" {...props} />
+}
+
+function DrawerPortal({
+    ...props
+}: React.ComponentProps<typeof DrawerPrimitive.Portal>) {
+    return <DrawerPrimitive.Portal data-slot="drawer-portal" {...props} />
+}
+
+function DrawerOverlay({
+    className,
+    ...props
+}: React.ComponentProps<typeof DrawerPrimitive.Overlay>) {
+    return (
+        <DrawerPrimitive.Overlay
+            data-slot="drawer-overlay"
+            className={cn(
+                "fixed inset-0 z-50 bg-black/80 supports-backdrop-filter:backdrop-blur-xs data-open:animate-in data-open:fade-in-0 data-closed:animate-out data-closed:fade-out-0",
+                className
+            )}
+            {...props}
+        />
+    )
+}
+
+function DrawerContent({
+    className,
+    children,
+    ...props
+}: React.ComponentProps<typeof DrawerPrimitive.Content>) {
+    return (
+        <DrawerPortal data-slot="drawer-portal">
+            <DrawerOverlay />
+            <DrawerPrimitive.Content
+                data-slot="drawer-content"
+                className={cn(
+                    "group/drawer-content fixed z-50 flex h-auto flex-col bg-transparent p-8 text-sm text-popover-foreground before:absolute before:inset-2 before:-z-10 before:rounded-4xl before:border before:border-border before:bg-popover data-[vaul-drawer-direction=bottom]:inset-x-0 data-[vaul-drawer-direction=bottom]:bottom-0 data-[vaul-drawer-direction=bottom]:mt-24 data-[vaul-drawer-direction=bottom]:max-h-[80vh] data-[vaul-drawer-direction=left]:inset-y-0 data-[vaul-drawer-direction=left]:left-0 data-[vaul-drawer-direction=left]:w-3/4 data-[vaul-drawer-direction=right]:inset-y-0 data-[vaul-drawer-direction=right]:right-0 data-[vaul-drawer-direction=right]:w-3/4 data-[vaul-drawer-direction=top]:inset-x-0 data-[vaul-drawer-direction=top]:top-0 data-[vaul-drawer-direction=top]:mb-24 data-[vaul-drawer-direction=top]:max-h-[80vh] data-[vaul-drawer-direction=left]:sm:max-w-sm data-[vaul-drawer-direction=right]:sm:max-w-sm",
+                    className
+                )}
+                {...props}
+            >
+                {children}
+            </DrawerPrimitive.Content>
+        </DrawerPortal>
+    )
+}
+
+export {
+    Drawer,
+    DrawerContent,
+    DrawerTrigger,
+}
