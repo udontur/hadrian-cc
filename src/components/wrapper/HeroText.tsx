83da0b5fa742539750ec02d7a3bf281cb423f5eb
Hadrian <h@hadrian.cc>
2026-06-24 09:25:08 +0800

    opt: bye bye useless code

--- src/components/wrapper/HeroText.tsx
+++ src/components/wrapper/HeroText.tsx
@@ -1,39 +1,17 @@
 import { BlurReveal } from "@/components/ui/blur-reveal.tsx"
-import { SpecialText } from "@/components/ui/special-text.tsx"
-
 
 export function Name({
     text,
     className = "",
-    speed,
 }: {
     text: string
-    speed?: number
     className?: string
 }) {
     return (
         <div>
-            <BlurReveal className={className} speedReveal={speed} inView>
+            <BlurReveal className={className} speedReveal={0.2} inView>
                 {text}
             </BlurReveal>
-        </div>
-    )
-}
-
-export function UserNameFlash({
-    text,
-    className = "",
-    speed,
-}: {
-    text: string
-    className?: string
-    speed: number
-}) {
-    return (
-        <div>
-            <SpecialText className={className} speed={speed} inView>
-                {text}
-            </SpecialText>
         </div>
     )
 }
