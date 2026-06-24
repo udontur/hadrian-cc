b2ed07a8d4aee516579d67305a60ef92dd9c2205
Hadrian <h@hadrian.cc>
2026-06-24 12:01:54 +0800

    opt: optimization

--- src/components/wrapper/OrbDrawer.tsx
+++ src/components/wrapper/OrbDrawer.tsx
@@ -17,25 +17,27 @@ export function OrbDrawer({
     return (
         <Drawer>
             <DrawerTrigger asChild>
-                <button>
+                <button className="cursor-pointer">
                     <img
                         src={coverImage.src}
                         alt={title}
+                        className="rounded-full border-solid"
                         width={64}
                         height={64}
                     />
                 </button>
             </DrawerTrigger>
-            <DrawerContent>
-                <div>
+            <DrawerContent className="flex items-center">
+                <div className="flex max-w-3xl gap-8">
                     <img
                         src={contentImage.src}
                         alt={title}
+                            className="rounded-2xl border-solid border-2 border-primary inset-shadow-2xl inset-shadow-primary"
                         width={256}
                         height={256}
                     />
-                    <div>
-                        <div>{title}</div>
+                    <div className="flex flex-col gap-4 mt-2">
+                        <div className="font-heading text-4xl underline decoration-primary decoration-wavy font-bold">{title}</div>
                         <div>{description}</div>
                     </div>
                 </div>
